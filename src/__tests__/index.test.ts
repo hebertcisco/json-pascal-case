57793a1335d84395a4781b6f8a9c8ccdd5827d3e
hebertcisco <hebertcisco@outlook.com>
2022-10-09 17:58:40 -0300

    test: :white_check_mark: Adding test to JsonPascalCase class

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,21 +1,49 @@
-import JsonPascalCase from '../index';
+import { JsonPascalCase } from '../index';
+import { MockJsonPascalCase } from '../shared/mocks/core/JsonPascalCase.mock';
 
 describe('JsonPascalCase', () => {
+  let jsonPascalCase: JsonPascalCase;
   let myObject: object;
 
   beforeAll(() => {
+    jsonPascalCase = new MockJsonPascalCase();
     myObject = {
       name: 'foo',
       surname: 'bar',
     };
   });
 
-  test('should match a valid UUID', () => {
-    const expectResult = {
-      Name: 'foo',
-      Surname: 'bar',
-    };
-    const result = JsonPascalCase.transformObject(myObject);
-    expect(result).toBeDefined();
+  describe('transformObject', () => {
+    it('should return an object', () => {
+      const result = jsonPascalCase.transformObject(myObject);
+      expect(result).toEqual({
+        Name: 'foo',
+        Surname: 'bar',
+      });
+    });
+  });
+  describe('transformArrayOfObjects', () => {
+    it('should return an array of objects', () => {
+      const result = jsonPascalCase.transformArrayOfObjects([myObject]);
+      expect(result).toEqual([
+        {
+          Name: 'foo',
+          Surname: 'bar',
+        },
+      ]);
+    });
+  });
+
+  describe('transformString', () => {
+    it('should return a string', () => {
+      const result = jsonPascalCase.transformString('foo');
+      expect(result).toEqual('Foo');
+    });
+  });
+  describe('transformArrayOfStrings', () => {
+    it('should return an array of strings', () => {
+      const result = jsonPascalCase.transformArrayOfStrings(['foo']);
+      expect(result).toEqual(['Foo']);
+    });
   });
 });
